196b78a3e5f56a675f5f86b36fcf15ae750aa815
eduardoitoeste <eduardoitoeste@gmail.com>
2020-11-16 16:24:25 -0300

    first commit

--- src/Socket.php
+++ src/Socket.php
@@ -2,14 +2,15 @@
 namespace itoeste\socket;
 use Exception;
 Class Socket {
-	$this->route = "192.168.1.8:3333";
-	$this->token = null
-	function __construct($options){
-		if(!isset(env("APIKEY_ITOESTESOCKET"))){
+	private $route = "192.168.1.8:3333";
+	private $token = null;
+	function __construct(){
+
+		if(!env("APIKEY_ITOESTESOCKET")){
 			throw new Exception("Falta la variable APIKEY_ITOESTESOCKET en el archivo env",1);
 		}
 
-		if(empty(env("APIKEY_ITOESTESOCKET")){
+		if( empty(env("APIKEY_ITOESTESOCKET"))) {
 			throw new Exception("La variable APIKEY_ITOESTESOCKET esta vacia",2);
 		}
 
@@ -18,10 +19,61 @@ Class Socket {
 
 	public function Notification($data)
 	{
+        $ruta = "http://".$this->route."/temp";
 		$formJson = json_encode($data);
 		$curl = curl_init();
-		curl_setopt_array($curl2, array(
-            CURLOPT_URL => $this->route."/temp",
+		curl_setopt_array($curl, array(
+            CURLOPT_URL => $ruta,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $formJson,
+            CURLOPT_HTTPHEADER => array(
+            "accept: application/json",
+            "api-key: ".$this->token,
+            "content-type: application/json"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        if($err){
+        	throw new Exception("Error de curl",3);
+        }
+
+        $responseJson2 = json_decode($response);
+        return true;
+	}
+<?php 
+namespace itoeste\socket;
+use Exception;
+Class Socket {
+	private $route = "192.168.1.8:3333";
+	private $token = null;
+	function __construct(){
+
+		if(!env("APIKEY_ITOESTESOCKET")){
+			throw new Exception("Falta la variable APIKEY_ITOESTESOCKET en el archivo env",1);
+		}
+
+		if( empty(env("APIKEY_ITOESTESOCKET"))) {
+			throw new Exception("La variable APIKEY_ITOESTESOCKET esta vacia",2);
+		}
+
+		$this->token = env("APIKEY_ITOESTESOCKET");
+	}
+
+	public function Notification($data)
+	{
+        $ruta = "http://".$this->route."/temp";
+		$formJson = json_encode($data);
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+            CURLOPT_URL => $ruta,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
